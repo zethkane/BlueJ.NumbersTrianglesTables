a3abfeaa86d42ed059ead46bc541cd80b43e4440
Zeth Kane <zkane@zipcodes-MacBook-Pro-2.local>
2020-02-05 18:52:59 -0500

    Finished the methods to satisfy all the tests

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,50 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String range = "";
+        for (int i = 1; i <= 5; i++){
+            for ( int j = 1; j <= 5; j++){
+               if (i*j < 10) 
+               {range += "  "+ i*j +" |";}
+               else if ((i*j >= 10) && (i*j < 100))
+               {range += " " + i*j +" |";}
+              
+                }range += "\n"; 
+        }
+                return range;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String range = "";
+        for (int i = 1; i <= 10; i++){
+            for ( int j = 1; j <= 10; j++){
+               if (i*j < 10) 
+               {range += "  "+ i*j +" |";}
+               else if ((i*j >= 10) && (i*j < 100))
+               {range += " " + i*j +" |";}
+               else {
+                   range += i*j + " |";}
+                 
+               
+                }range += "\n"; 
+        }
+                return range;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String range = "";
+        for (int i = 1; i <= tableSize; i++){
+            for ( int j = 1; j <= tableSize; j++){
+               if (i*j < 10) 
+               {range += "  "+ i*j +" |";}
+               else if ((i*j >= 10) && (i*j < 100))
+               {range += " " + i*j +" |";}
+               else {
+                   range += i*j + " |";}
+                 
+               
+                }range += "\n"; 
+        }
+                return range;
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,43 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String range = "";
+        for (int i = 1; i <= numberOfStars; i++){
+            range += "*";
+        }    
+        return range;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String range = "";
+        for (int i = 1; i <= numberOfRows; i++){
+            for ( int j = 1; j <= i; j++){
+                range += "*";}
+                  range += "\n";
+          }
+                return range;  
     }
+    
+    
 
 
     public static String getSmallTriangle() {
-        return null;
+        String range = "";
+        for (int i = 1; i <= 4 ; i++){
+            for ( int j = 1; j <= i; j++){
+                range += "*";}
+                  range += "\n";
+          }
+                return range; 
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String range = "";
+        for (int i = 1; i <= 9; i++){
+            for ( int j = 1; j <= i; j++){
+                range += "*";}
+                  range += "\n";
+          }
+                return range; 
     }
 }
